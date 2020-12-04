e73f342e6624a69899c8d1aa3e092a6a0f3b572c
DAVID LEAL <dl0885@students.necsd.net>
2020-12-04 15:11:00 +0000

    did questions 1-4:D

--- Prob1.java
+++ Prob1.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 
 public class Prob1{
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter two integers: ");
     
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+
+    if (num1==num2)
+      System.out.println("Yes");
 
 
     
--- Prob4.java
+++ Prob4.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What is the temperature?");
+
+    double temp = scan.nextDouble();
+
+    if(temp < 99 || temp > 102) {
+      System.out.println("WARNING");
+    }
+
+    else
+      System.out.println("Temperature is OK");
     
 
 
